83d5654892b9dee273f72946e0c53e6a338572d1
Sivashankar G <sivashankar.g@8kmiles.com>
2014-08-30 01:06:19 +0530

    pdf parse

---
+++ src/com/hadoop/white/ch7/examples/PDFToTextConverterJob.java
@@ -0,0 +1,65 @@
+package com.hadoop.white.ch7.examples;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+
+public class PDFToTextConverterJob extends Configured implements Tool {
+	
+	public static class PDFMapper extends Mapper<NullWritable,BytesWritable,NullWritable,Text> {
+		
+		
+		@Override
+		protected void map(NullWritable key, BytesWritable value, Context context) throws IOException, InterruptedException {
+			context.write(key, new Text(value.getBytes()));
+		}
+		
+	}
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		
+		int result = ToolRunner.run(new PDFToTextConverterJob(), args);
+        System.exit(result);
+
+	}
+
+	@Override
+	public int run(String[] args) throws Exception {
+		Configuration conf = getConf();
+		Job job = new Job(conf);
+		
+		job.setJarByClass(PDFToTextConverterJob.class);
+		job.setJobName("pdf2text");
+		
+		job.setInputFormatClass(WholeFileFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setOutputKeyClass(NullWritable.class);
+		job.setOutputValueClass(Text.class);
+		job.setMapperClass(PDFMapper.class);
+		job.setMapOutputKeyClass(NullWritable.class);
+		job.setMapOutputValueClass(Text.class);
+		job.setNumReduceTasks(0);
+		
+		WholeFileFormat.setInputPaths(job, new Path(args[0]));
+		TextOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
+
+}
