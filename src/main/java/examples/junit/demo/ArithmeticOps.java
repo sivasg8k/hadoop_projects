4e6059118e6242cf83f19012ecdfd401a2ad1b5f
Sivashankar <sivashankar.g@8kmiles.com>
2015-03-22 21:15:59 -0500

    junit jenkins demo

---
+++ src/main/java/examples/junit/demo/ArithmeticOps.java
@@ -0,0 +1,21 @@
+package examples.junit.demo;
+
+public class ArithmeticOps {
+
+	public int add(int a, int b) {
+		return (a + b);
+	}
+	
+	public int sub(int a, int b) {
+		return (a - b);
+	}
+	
+	public int multiply(int a, int b) {
+		return (a * b);
+	}
+	
+	public int divide(int a, int b) {
+		return (a/b);
+	}
+	
+}
---
+++ src/test/java/examples/junit/demo/TestArithmeticOps.java
@@ -0,0 +1,45 @@
+package examples.junit.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestArithmeticOps {
+	
+	ArithmeticOps arithOps = null;
+
+	@Before
+	public void setUp() throws Exception {
+		
+		arithOps = new ArithmeticOps();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		
+		arithOps = null;
+	}
+
+	@Test
+	public void testAdd() {
+		assertEquals(11,arithOps.add(5, 6)); 
+	}
+
+	@Test
+	public void testSub() {
+		assertEquals(1,arithOps.sub(6, 5)); 
+	}
+
+	@Test
+	public void testMultiply() {
+		assertEquals(30,arithOps.multiply(5, 6)); 
+	}
+
+	@Test
+	public void testDivide() {
+		assertEquals(2,arithOps.divide(6, 3));
+	}
+
+}
